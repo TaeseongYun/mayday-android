a37a4f1669715a745771351dc6f2576ab960410d
developer-sungjae <dunkey2615@naver.com>
2020-08-03 01:57:45 +0900

    룸 디버깅

--- local/src/main/java/com/project/local/model/LocalDao.kt
+++ local/src/main/java/com/project/local/model/LocalDao.kt
@@ -7,16 +7,16 @@ import androidx.room.Query
 import io.reactivex.Completable
 import io.reactivex.Maybe
 
-//@Dao
-//interface LocalDao {
-//
-//    @Query("SELECT * FROM list")
-//    fun getList(): Maybe<LocalItem>
-//
-//    /* import android.arch.persistence.room.OnConflictStrategy.REPLACE */
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insert(item: LocalItem): Completable
-//
-//    @Query("DELETE from list")
-//    fun deleteAll()
-//}
+@Dao
+interface LocalDao {
+
+    @Query("SELECT * FROM list")
+    fun getList(): Maybe<MayDayRoomEntity>
+
+    /* import android.arch.persistence.room.OnConflictStrategy.REPLACE */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(item: MayDayRoomEntity): Completable
+
+    @Query("DELETE from list")
+    fun deleteAll()
+}
