85b384199b718e342bf498fe92ce93b96b7a1c09
TaeseongYun <yts0646@gmail.com>
2020-08-11 19:34:23 +0900

    ISSUE #16 feat : ThrottleFirstClickListner

---
+++ content/src/main/java/com/project/content/ext/ThrottleFirstClickListener.kt
@@ -0,0 +1,32 @@
+package com.project.content.ext
+
+import android.view.View
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.BehaviorSubject
+import java.util.concurrent.TimeUnit
+
+typealias ClickListener = (View) -> Unit
+
+class ThrottleFirstClickListener(
+    disposable: CompositeDisposable,
+    private val listener: ClickListener
+) : View.OnClickListener {
+    private val onClickListenerBehaviorSubject = BehaviorSubject.create<View>()
+
+    init {
+
+        onClickListenerBehaviorSubject.throttleFirst(1000L, TimeUnit.MICROSECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                it.run(listener)
+            }
+            .addTo(disposable)
+    }
+
+    override fun onClick(view: View) {
+        onClickListenerBehaviorSubject.onNext(view)
+    }
+
+}
