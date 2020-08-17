7079908df1b6c34b56c1ec96444ffc5d6e1bd6b7
TaeseongYun <yts0646@gmail.com>
2020-08-17 22:47:25 +0900

    modify : toast 디폴트 duration 설정

--- content/src/main/java/com/project/content/ext/WidgetExt.kt
+++ content/src/main/java/com/project/content/ext/WidgetExt.kt
@@ -3,6 +3,6 @@ package com.project.content.ext
 import android.content.Context
 import android.widget.Toast
 
-fun Context.toast(message: String, duration: Int) =
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_LONG) =
     Toast.makeText(this, message, duration)
         .show()
